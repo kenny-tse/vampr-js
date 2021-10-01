e41b0d70db2360e803d9178c06988ea9692a0f0c
kenny-tse <kennytse91@gmail.com>
2021-10-01 20:00:51 +0000

    added function bodies to vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,48 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    vampire.creator = this;
+    this.offspring.push(vampire);
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
 
+    let numberOfVampires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
 
+    if (this.creator === null) {
+      return true;
+    }
+
+    if (vampire.creator === null) {
+      return false;
+    }
+
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      return false;
+    }
   }
 
   /** Stretch **/
